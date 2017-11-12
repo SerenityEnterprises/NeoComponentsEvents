a3cb438b9cacb94f5beab943675223971591f2dc
half cambodian hacker man <anthonyjoshuasom@gmail.com>
2017-11-12 22:51:39 +0000

    Kotlin extensions - Release 2.1.0

--- src/main/kotlin/host/serenity/neo/components/events/Extensions.kt
+++ src/main/kotlin/host/serenity/neo/components/events/Extensions.kt
@@ -1,10 +1,19 @@
 package host.serenity.neo.components.events
 
 import host.serenity.neo.components.events.util.GenericInfo
+import kotlin.reflect.KClass
 
 fun <T : Event> EventBus.register(listener: Listener<T>) {
     val eventClass = GenericInfo.getGenericType(listener.javaClass)
 
     @Suppress("UNCHECKED_CAST")
     register(eventClass as Class<T>, listener)
+}
+
+fun <T : Event> EventBus.register(type: KClass<T>, listener: (T) -> Unit) {
+    register(type.java, object: Listener<T> {
+        override fun fire(event: T) {
+            listener(event)
+        }
+    })
 }
