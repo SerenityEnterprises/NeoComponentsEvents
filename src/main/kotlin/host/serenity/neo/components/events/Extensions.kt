60292f52dbdde24bcb333adb420cadc37eaa2a60
half cambodian hacker man <anthonyjoshuasom@gmail.com>
2017-11-12 22:55:46 +0000

    Return IDs on extension methods

--- src/main/kotlin/host/serenity/neo/components/events/Extensions.kt
+++ src/main/kotlin/host/serenity/neo/components/events/Extensions.kt
@@ -3,15 +3,15 @@ package host.serenity.neo.components.events
 import host.serenity.neo.components.events.util.GenericInfo
 import kotlin.reflect.KClass
 
-fun <T : Event> EventBus.register(listener: Listener<T>) {
+fun <T : Event> EventBus.register(listener: Listener<T>): Int {
     val eventClass = GenericInfo.getGenericType(listener.javaClass)
 
     @Suppress("UNCHECKED_CAST")
-    register(eventClass as Class<T>, listener)
+    return register(eventClass as Class<T>, listener)
 }
 
-fun <T : Event> EventBus.register(type: KClass<T>, listener: (T) -> Unit) {
-    register(type.java, object: Listener<T> {
+fun <T : Event> EventBus.register(type: KClass<T>, listener: (T) -> Unit): Int {
+    return register(type.java, object: Listener<T> {
         override fun fire(event: T) {
             listener(event)
         }
